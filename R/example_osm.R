cdf0e66e9bc564f9c5f7a8f76821b32ae49ad246
LOUISE-DESKTOP\Louise <louise.mcmillan@gmail.com>
2019-06-04 12:37:10 +1200

    Changed script for testing old and new OSM code to input initial pi value and to switch off reparametrization in original OSM code.

--- R/example_osm.R
+++ R/example_osm.R
@@ -10,7 +10,10 @@ set.seed(1)
 results <- osmrowclustering("Y~row",
                             nclus.row=2,
                             y.mat=y.mat.sim,
+                            pi.init=c(0.1,0.9),
                             use.model.without.interactions = TRUE)
+
+
 
 results$info
 results$initvect
@@ -18,16 +21,18 @@ results$initvect
 #Initialize parameters for Daniel's original OSM code
 # Daniel's original OSM code has RG-1 of the pi values as the last part of row
 # clustering retval
-parstart <- c(results$initvect,results$pi[1])
+parstart <- c(results$initvect,0.1)
 
 y.mat <- y.mat.sim
 min.numRows.to.test <- 2
 max.numRows.to.test <- 2
-reparC <- 1
+reparC <- 0 ## Reparametrization is NOT coded into new OSM yet so don't want to use it
 arraydata <- as.vector(y.mat)
 path.C.funcs <- "src/"
 path.results <- ""
 namefile <- "simulated_5by100"
 set.seed(1)
-RowCluster.rRcC1(scale.pars=FALSE, type=0, polish=FALSE, R=2, parstart=parstart)
+results <- RowCluster.rRcC1(scale.pars=FALSE, type=0, polish=FALSE, R=2, parstart=parstart)
 
+### TEMPORARY!!! CHANGED OPTIM settings in utils_daniel_osm.R to match the settings
+### used for optim in OSMrowclustering
