dcd5df5850e5f42d891d8c1f7e8ecd593a674880
lfmcmillan <louise.mcmillan@gmail.com>
2019-06-07 15:59:05 +1200

    Commit of clustering.R which contains row, column and biclustering for POM and OSM.

--- R/example_biclustering.R
+++ R/example_biclustering.R
@@ -1,22 +1,17 @@
-source("R/POMbiclustering.R")
-source("R/POMrowclustering.R")
-source("R/OSMrowclustering.R")
+source("R/clustering.R")
 source("R/likelihoods_memberships.R")
 source("R/utils.R")
 
 set.seed(100)
 y.mat.sim <- matrix(sample(1:3,5*100,replace=TRUE),nrow=100)
 set.seed(1)
-results <- pombiclustering("Y~row+column+row:column",
-                            nclus.row=2,nclus.column=2,
-                            y.mat=y.mat.sim, use.matrix=TRUE)
-
-results <- pomrowclustering("Y~row+column+row:column",
-                nclus.row=2,
-                y.mat=y.mat.sim,
-                use.alternative.start = TRUE)
-
-results <- osmrowclustering("Y~row",
+results <- rowclustering("Y~row+column+row:column",model="OSM",
                             nclus.row=2,
-                            y.mat=y.mat.sim,
-                            use.alternative.start = TRUE)
+                            y.mat=y.mat.sim)
+results <- rowclustering("Y~row+column+row:column",model="POM",
+                            nclus.row=2,
+                            y.mat=y.mat.sim)
+
+results <- biclustering("Y~row+column+row:column",model="OSM",
+                            nclus.row=2,nclus.column=2,
+                            y.mat=y.mat.sim)
