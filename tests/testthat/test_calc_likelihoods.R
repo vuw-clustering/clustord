1eaa3e9d60f9d96a8e3cee5355c9b39dc6a796af
LOUISE-DESKTOP\Louise <louise.mcmillan@gmail.com>
2019-07-02 15:17:09 +1200

    Fixed incorrect tests in test_calc_likelihoods that needed y.mat as well as long.df inputs.

--- tests/testthat/test_calc_likelihoods.R
+++ tests/testthat/test_calc_likelihoods.R
@@ -20,47 +20,48 @@ test_that("calc.ll produces correct results.", {
 
     long.df <- data.frame(Y=factor(c(c(1,1,1,2,2,2),c(1,1,2,2,3,3),c(1,2,3,1,2,3))),
                           ROW=rep(1:6,times=3), COL=rep(1:3,each=6))
+    y.mat <- df2mat(long.df)
 
     ## OSM
-    expect_equivalent(calc.ll(c(mu,phi,alpha_r), long.df, "OSM", "rs", ppr.m, pi.v, RG,
+    expect_equivalent(calc.ll(c(mu,phi,alpha_r), long.df, y.mat, "OSM", "rs", ppr.m, pi.v, RG,
                       constraint.sum.zero=TRUE, partial=TRUE),
                       30.77587457, tolerance=1E-4)
-    expect_equivalent(calc.ll(c(mu,phi,alpha_r,beta_j), long.df, "OSM", "rp", ppr.m, pi.v, RG,
+    expect_equivalent(calc.ll(c(mu,phi,alpha_r,beta_j), long.df, y.mat, "OSM", "rp", ppr.m, pi.v, RG,
                               constraint.sum.zero=TRUE, partial=TRUE),
                       35.08129788, tolerance=1E-4)
-    expect_equivalent(calc.ll(c(mu,phi,alpha_r,beta_j,gamma_rj), long.df, "OSM", "rpi", ppr.m, pi.v, RG,
+    expect_equivalent(calc.ll(c(mu,phi,alpha_r,beta_j,gamma_rj), long.df, y.mat, "OSM", "rpi", ppr.m, pi.v, RG,
                               constraint.sum.zero=TRUE, partial=TRUE),
                       37.2425604, tolerance=1E-4)
 
     beta_c <- c(0.5)
     gamma_rc <- c(0.5)
 
-    expect_equivalent(calc.ll(c(mu,phi,alpha_r,beta_c), long.df, "OSM", "rc",
+    expect_equivalent(calc.ll(c(mu,phi,alpha_r,beta_c), long.df, y.mat, "OSM", "rc",
                               ppr.m, pi.v, RG, ppc.m, kappa.v, CG,
                               constraint.sum.zero=TRUE, partial=TRUE),
                       31.72322174, tolerance=1E-4)
-    expect_equivalent(calc.ll(c(mu,phi,alpha_r,beta_c,gamma_rc), long.df, "OSM", "rci",
+    expect_equivalent(calc.ll(c(mu,phi,alpha_r,beta_c,gamma_rc), long.df, y.mat, "OSM", "rci",
                               ppr.m, pi.v, RG, ppc.m, kappa.v, CG,
                               constraint.sum.zero=TRUE, partial=TRUE),
                       32.7504653, tolerance=1E-4)
 
 
     ## POM
-    expect_equivalent(calc.ll(c(mu,alpha_r), long.df, "POM", "rs", ppr.m, pi.v, RG,
+    expect_equivalent(calc.ll(c(mu,alpha_r), long.df, y.mat, "POM", "rs", ppr.m, pi.v, RG,
                               constraint.sum.zero=TRUE, partial=TRUE),
                       25.93596493, tolerance=1E-4)
-    expect_equivalent(calc.ll(c(mu,alpha_r,beta_j), long.df, "POM", "rp", ppr.m, pi.v, RG,
+    expect_equivalent(calc.ll(c(mu,alpha_r,beta_j), long.df, y.mat, "POM", "rp", ppr.m, pi.v, RG,
                               constraint.sum.zero=TRUE, partial=TRUE),
                       31.35665815, tolerance=1E-4)
-    expect_equivalent(calc.ll(c(mu,alpha_r,beta_j,gamma_rj), long.df, "POM", "rpi", ppr.m, pi.v, RG,
+    expect_equivalent(calc.ll(c(mu,alpha_r,beta_j,gamma_rj), long.df, y.mat, "POM", "rpi", ppr.m, pi.v, RG,
                               constraint.sum.zero=TRUE, partial=TRUE),
                       33.29262377, tolerance=1E-4)
 
-    expect_equivalent(calc.ll(c(mu,alpha_r,beta_c), long.df, "POM", "rc",
+    expect_equivalent(calc.ll(c(mu,alpha_r,beta_c), long.df, y.mat, "POM", "rc",
                               ppr.m, pi.v, RG, ppc.m, kappa.v, CG,
                               constraint.sum.zero=TRUE, partial=TRUE),
                       26.79095153, tolerance=1E-4)
-    expect_equivalent(calc.ll(c(mu,alpha_r,beta_c,gamma_rc), long.df, "POM", "rci",
+    expect_equivalent(calc.ll(c(mu,alpha_r,beta_c,gamma_rc), long.df, y.mat, "POM", "rci",
                               ppr.m, pi.v, RG, ppc.m, kappa.v, CG,
                               constraint.sum.zero=TRUE, partial=TRUE),
                       28.61441617, tolerance=1E-4)
