e3e74b1acb60f40ebbe8172a69d7d8d174a6929e
LOUISE-DESKTOP\Louise <louise.mcmillan@gmail.com>
2019-07-08 16:47:03 +1200

    Simplified generate start code, and changed to only use MASS::polr for POM.

--- R/generatestart.R
+++ R/generatestart.R
@@ -9,15 +9,8 @@ generate.start.rowcluster <- function(long.df, model, submodel, RG, initvect=NUL
 
     q <- length(levels(long.df$Y))
 
+    ## Generate initvect -------------------------------------------------------
     if (is.null(initvect)) {
-
-        PO.sp.out <- MASS::polr(Y~1,data=long.df)
-        PO.sp.out$mu=PO.sp.out$zeta
-
-        PO.sp.out <- MASS::polr(Y~as.factor(COL),data=long.df)
-        PO.sp.out$mu=PO.sp.out$zeta
-
-        mu.init=PO.sp.out$mu
 
         if (all(table(long.df[,c("ROW","COL")]) == 1)) {
 
@@ -45,56 +38,53 @@ generate.start.rowcluster <- function(long.df, model, submodel, RG, initvect=NUL
                    # phi.init <- sort(runif(q-2),decreasing=FALSE)
                    u.init <- runif(q-2,min=-1,max=1)
 
+                   PO.sp.out <- MASS::polr(Y~as.factor(COL),data=long.df)
+                   PO.sp.out$mu=PO.sp.out$zeta
+
+                   ## If not using constraint that beta sum to zero,
+                   ## beta1 will be 0 so need to correct other elements
+                   ## of beta accordingly
+                   if (constraint.sum.zero) beta.init <- PO.sp.out$coef[1:(p-1)]
+                   else beta.init <- PO.sp.out$coef[2:(p-1)] - PO.sp.out$coef[1]
+
                    switch(submodel,
                           "rs"={
                               initvect <- c(mu.init, u.init, alpha.init)
                           },
                           "rp"={
-                              ## If not using constraint that beta sum to zero,
-                              ## beta1 will be 0 so need to correct other elements
-                              ## of beta accordingly
-                              if (constraint.sum.zero) beta.init <- PO.sp.out$coef[1:(p-1)]
-                              else beta.init <- PO.sp.out$coef[2:(p-1)] - PO.sp.out$coef[1]
-
                               initvect <- c(mu.init, u.init, alpha.init, beta.init)
                           },
                           "rpi"={
-                              ## If not using constraint that beta sum to zero,
-                              ## beta1 will be 0 so need to correct other elements
-                              ## of beta accordingly
-                              if (constraint.sum.zero) beta.init <- PO.sp.out$coef[1:(p-1)]
-                              else beta.init <- PO.sp.out$coef[2:(p-1)] - PO.sp.out$coef[1]
-
                               gamma.init <- rep(0.1,(RG-1)*(p-1))
 
                               initvect <- c(mu.init,u.init,alpha.init,beta.init,gamma.init)
                           },stop("Invalid model for row/column clustering"))
                },
                "POM"={
+                   PO.sp.out <- MASS::polr(Y~as.factor(COL),data=long.df)
+                   mu.init <- PO.sp.out$zeta
+
+                   ## If not using constraint that beta sum to zero,
+                   ## beta1 will be 0 so need to correct other elements
+                   ## of beta accordingly
+                   if (constraint.sum.zero) beta.init <- PO.sp.out$coef[1:(p-1)]
+                   else beta.init <- PO.sp.out$coef[2:(p-1)] - PO.sp.out$coef[1]
+
                    switch(submodel,
                           "rs"={
                               initvect <- c(mu.init,alpha.init)
                           },
                           "rp"={
-                              ## If not using constraint that beta sum to zero,
-                              ## beta1 will be 0 so need to correct other elements
-                              ## of beta accordingly
-                              if (constraint.sum.zero) beta.init <- PO.sp.out$coef[1:(p-1)]
-                              else beta.init <- PO.sp.out$coef[2:(p-1)] - PO.sp.out$coef[1]
                               initvect <- c(mu.init,alpha.init,beta.init)
                           },
                           "rpi"={
-                              ## If not using constraint that beta sum to zero,
-                              ## beta1 will be 0 so need to correct other elements
-                              ## of beta accordingly
-                              if (constraint.sum.zero) beta.init <- PO.sp.out$coef[1:(p-1)]
-                              else beta.init <- PO.sp.out$coef[2:(p-1)] - PO.sp.out$coef[1]
                               gamma.init <- rep(0.1,(RG-1)*(p-1))
                               initvect <- c(mu.init,alpha.init,beta.init,gamma.init)
                           },stop("Invalid model for row/column clustering"))
                })
     }
 
+    ## Generate pi.init --------------------------------------------------------
     if (is.null(pi.init)) {
         if (exists("pi.kmeans") && !is.null(pi.kmeans))
         {
@@ -103,140 +93,26 @@ generate.start.rowcluster <- function(long.df, model, submodel, RG, initvect=NUL
             pi.init <- runif(RG-1,0,1/RG)
             pi.init <- c(pi.init,1-sum(pi.init))
         }
-        switch(model,
-               "OSM"={
-                   switch(submodel,
-                          "rs"={
-                              ## No need to make any further changes to pi.init
-                          },
-                          "rp"={
-                              startEM.control <- list(EMcycles=EM.control$startEMcycles,
-                                                      EMstoppingpar=EM.control$EMstoppingpar,
-                                                      paramstopping=EM.control$paramstopping)
 
-                              cat("Fitting RS model to obtain starting values for pi.v\n")
-                              OSM.rs.out <- run.EM.rowcluster(invect=initvect[1:(q-1+q-2+RG-1)],
-                                                              long.df, model="OSM",submodel="rs",
-                                                              pi.v=pi.init,
-                                                              EM.control=startEM.control,
-                                                              optim.method=optim.method,
-                                                              optim.control=optim.control)
-                              cat("=== End of RS model fitting ===\n")
+        startEM.control <- list(EMcycles=EM.control$startEMcycles,
+                                EMstoppingpar=EM.control$EMstoppingpar,
+                                paramstopping=EM.control$paramstopping)
 
-                              pi.init <- OSM.rs.out$pi
-                          },
-                          "rpi"={
-                              startEM.control <- list(EMcycles=EM.control$startEMcycles,
-                                                      EMstoppingpar=EM.control$EMstoppingpar,
-                                                      paramstopping=EM.control$paramstopping)
-                              if (use.alternative.start) {
+        if (submodel %in% c("rp","rpi")) {
+            cat("Fitting RS model to obtain starting values for pi.v\n")
+            invect <- switch(model,
+                             "OSM"=initvect[1:(q-1+q-2+RG-1)],
+                             "POM"=initvect[1:(q-1+RG-1)])
 
-                                  OSM.rp.out <- run.EM.rowcluster(invect=initvect[1:(q-1+q-2+RG-1+p-1)],
-                                                                  long.df, model="OSM",submodel="rp",
-                                                                  pi.v=pi.init, EM.control=startEM.control,
-                                                                  optim.method=optim.method,
-                                                                  optim.control=optim.control)
-                                  cat("=== End of RP model fitting ===\n")
-
-                                  ppr.m=OSM.rp.out$ppr
-                                  pi.init=OSM.rp.out$pi
-                              } else {
-                                  PO.ss.out <- MASS::polr(Y~1,data=long.df)
-                                  PO.ss.out$mu <- PO.ss.out$zeta
-
-                                  PO.sp.out <- MASS::polr(Y~as.factor(COL),data=long.df)
-                                  PO.sp.out$beta <- PO.sp.out$coef[1:(p-1)] #Individual column effect
-
-                                  if (constraint.sum.zero) beta.init <- PO.sp.out$coef[1:(p-1)]
-                                  else beta.init <- PO.sp.out$coef[2:(p-1)] - PO.sp.out$coef[1]
-
-                                  phi.init <- seq(from=runif(1,min=0.05,max=0.5),
-                                                  to=runif(1,min=0.6,max=0.95), length.out = (q-2))
-
-                                  OSM.rs.out <- run.EM.rowcluster(invect=c(PO.ss.out$mu,phi.init,alpha.kmeans[-RG]),
-                                                                  long.df, model="OSM",submodel="rs",
-                                                                  pi.v=pi.init, EM.control=startEM.control,
-                                                                  optim.method=optim.method,
-                                                                  optim.control=optim.control)
-                                  OSM.rp.out <- run.EM.rowcluster(invect=c(OSM.rs.out$parlist.out$mu,
-                                                                           phi.init,
-                                                                           OSM.rs.out$parlist.out$alpha[-RG],
-                                                                           beta.init),
-                                                                  long.df, model="OSM",submodel="rp",
-                                                                  pi.v=pi.init, EM.control=startEM.control,
-                                                                  optim.method=optim.method,
-                                                                  optim.control=optim.control)
-
-                                  ppr.m <- OSM.rp.out$ppr
-                                  pi.init <- OSM.rp.out$pi
-                                  cat("=== Used RS and RP models to find starting points ===\n")
-                              }
-                          })
-               },
-               "POM"={
-                   switch(submodel,
-                          "rs"={
-                              ## No need to make any further changes to pi.init
-                          },
-                          "rp"={
-                              startEM.control <- list(EMcycles=EM.control$startEMcycles,
-                                                      EMstoppingpar=EM.control$EMstoppingpar,
-                                                      paramstopping=EM.control$paramstopping)
-
-                              cat("Fitting RS model to obtain starting values for pi.v\n")
-                              POM.rs.out <- run.EM.rowcluster(invect=initvect[1:(q-1+RG-1)],
-                                                              long.df, model="POM",submodel="rs",
-                                                              pi.v=pi.init,
-                                                              EM.control=startEM.control,
-                                                              optim.method=optim.method,
-                                                              optim.control=optim.control)
-                              cat("=== End of RS model fitting ===\n")
-
-                              pi.init <- POM.rs.out$pi
-                          },
-                          "rpi"={
-                              startEM.control <- list(EMcycles=EM.control$startEMcycles,
-                                                      EMstoppingpar=EM.control$EMstoppingpar,
-                                                      paramstopping=EM.control$paramstopping)
-                              if (use.alternative.start) {
-
-                                  POM.rp.out <- run.EM.rowcluster(invect=initvect[1:(q-1+RG-1+p-1)],
-                                                                  long.df, model="POM",submodel="rp",
-                                                                  pi.v=pi.init, EM.control=startEM.control,
-                                                                  optim.method=optim.method,
-                                                                  optim.control=optim.control)
-                                  cat("=== End of RP model fitting ===\n")
-
-                                  ppr.m=POM.rp.out$ppr
-                                  pi.init=POM.rp.out$pi
-                              } else {
-                                  PO.ss.out <- MASS::polr(Y~1, data=long.df)
-                                  PO.ss.out$mu <- PO.ss.out$zeta
-
-                                  PO.sp.out <- MASS::polr(Y~as.factor(COL), data=long.df)
-
-                                  if (constraint.sum.zero) beta.init <- PO.sp.out$coef[1:(p-1)]
-                                  else beta.init <- PO.sp.out$coef[2:(p-1)] - PO.sp.out$coef[1]
-
-                                  POM.rs.out <- run.EM.rowcluster(invect=c(PO.ss.out$mu,alpha.kmeans[-RG]),
-                                                                  long.df, model="POM",submodel="rs",
-                                                                  pi.v=pi.init, EM.control=startEM.control,
-                                                                  optim.method=optim.method,
-                                                                  optim.control=optim.control)
-                                  POM.rp.out <- run.EM.rowcluster(invect=c(POM.rs.out$parlist.out$mu,
-                                                                           POM.rs.out$parlist.out$alpha[-RG],
-                                                                           beta.init),
-                                                                  long.df, model="POM",submodel="rp",
-                                                                  pi.v=pi.init, EM.control=startEM.control,
-                                                                  optim.method=optim.method,
-                                                                  optim.control=optim.control)
-
-                                  ppr.m <- POM.rp.out$ppr
-                                  pi.init <- POM.rp.out$pi
-                                  cat("=== Used RS and RP models to find starting points ===\n")
-                              }
-                          })
-               })
+            rs.out <- run.EM.rowcluster(invect=invect,
+                                        long.df, model=model,submodel="rs",
+                                        pi.v=pi.init,
+                                        EM.control=startEM.control,
+                                        optim.method=optim.method,
+                                        optim.control=optim.control)
+            cat("=== End of RS model fitting ===\n")
+            pi.init <- rs.out$pi
+        }
     }
 
     list(initvect=initvect, pi.init=pi.init)
@@ -253,12 +129,8 @@ generate.start.bicluster <- function(long.df, model, submodel, RG, CG,
 
     q <- length(levels(long.df$Y))
 
+    ## Generate initvect -------------------------------------------------------
     if (is.null(initvect)) {
-
-        PO.ss.out <- MASS::polr(Y~1,data=long.df)
-        PO.ss.out$mu <- PO.ss.out$zeta
-
-        mu.init <- PO.ss.out$mu
 
         if (all(table(long.df[,c("ROW","COL")]) == 1)) {
             ## convert to data matrix
@@ -307,6 +179,9 @@ generate.start.bicluster <- function(long.df, model, submodel, RG, CG,
                           },stop("Invalid model for biclustering"))
                },
                "POM"={
+                   PO.ss.out <- MASS::polr(Y~1,data=long.df)
+                   mu.init <- PO.ss.out$zeta
+
                    switch(submodel,
                           "rc"={
                               initvect <- c(mu.init,alpha.init,beta.init)
@@ -318,6 +193,7 @@ generate.start.bicluster <- function(long.df, model, submodel, RG, CG,
                })
     }
 
+    ## Generate pi.init and kappa.init -----------------------------------------
     if (is.null(pi.init)) generate.pi <- TRUE
     else generate.pi <- FALSE
     if (is.null(kappa.init)) generate.kappa <- TRUE
