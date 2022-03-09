ec665f9c94af869440b901d5800ee9c47cdfef47
lfmcmillan <louise.mcmillan@gmail.com>
2022-03-09 22:42:38 +1300

    Final documentation changes for v1.0

--- R/clustord.R
+++ R/clustord.R
@@ -4,22 +4,32 @@
 #' odds model (POM), ordered stereotype model (OSM) or binary model for ordinal
 #' categorical data.
 #'
-# TODO: REWRITE THIS!!! ====
+#' The clustord package provides five functions: \code{clustord.fit()},
+#' \code{mat2df()}, \code{calc.SE.rowcluster()}, \code{calc.SE.bicluster()}, and
+#' \code{calc.cluster.comparisons()}.
 #'
-#' The clustord package provides three important functions:
-#' rowclustering, columnclustering and biclustering.
-#'
-#' @section Clustering functions:
-#' All three clustering functions assume that you are starting from a data matrix,
+#' @section Clustering function:
+#' \code{clustord.fit} assumes that you started with a data matrix of responses,
 #' though you will need to convert that data matrix into a long-form data frame
-#' so that every element in the original data matrix becomes one row in the
-#' data frame, and the row and column indices from the data matrix become the
-#' columns ROW and COL in the data frame.
+#' before running \code{clustord.fit}. Every element in the original data matrix
+#' becomes one row in the data frame, and the row and column indices from the
+#' data matrix become the columns ROW and COL in the data frame.
 #'
-#' rowclustering clusters the rows of the matrix, and columnclustering clusters
-#' the columns of the matrix.
+#' @section Utility function:
+#' \code{mat2df()} is a utility function provided to convert a data matrix of
+#' responses into the long-form data frame format required by
+#' \code{clustord.fit()}, and can also attach any covariates to that long-form
+#' data frame if needed.
 #'
-#' biclustering clusters the rows and columns simultaneously.
+#' @section SE calculation functions:
+#' \code{calc.SE.rowcluster()} and \code{calc.SE.bicluster()} are functions to
+#' run after running \code{clustord.fit()}, to calculate the standard errors on
+#' the parameters fitted using \code{clustord.fit()}.
+#'
+#' @section Clustering comparisons:
+#' \code{calc.cluster.comparisons()} can be used to compare the assigned cluster
+#' memberships of the rows or columns of the data matrix from two different
+#' clustering fits, in a way that avoids the label-switching problem.
 #'
 #' @docType package
 #' @name clustord
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # clustord
 
-The three top-level functions are rowclustering, columnclustering and biclustering, all in the "clustering.R" file, and they have standard R-style manuals to explain their usage.
+Version 1.0 update released!
+
+The latest version of the package, version 1.0, is a major update from version 0.1. It now has the capacity to fit models including a variety of covariates, to make it consistent with the models that can be fitted with [clustglm](https://github.com/vuw-clustering/clustglm).
+
+Note that the input arguments and output components have changed. This version is not backwards compatible, i.e. you will not be able to run scripts for v0.1 using v1.0, but the changes that make it not backwards-compatible are mostly stylistic ones to avoid using "." notation. For example, the input argument `constraint.sum.zero` has now become `constraint_sum_zero`. This avoids potential confusion with S3 methods.
+
+There are five top-level functions. The main one is `clustord.fit()`, which performs clustering, including row clustering, column clustering, and biclustering. There is also an auxiliary one, `mat2df()`, which may need to be run before `clustord.fit()` to create the input data structure for `clustord.fit()` (see the `clustord.fit()` manual for details). The final two functions, `calc.SE.rowcluster()` and `calc.SE.bicluster()`, are designed to be run after `clustord.fit()`, to calculate the standard errors of the clustering parameters, if needed. `calc.cluster.comparisons()` can be used to compare the clustering results from different runs or different models, avoiding the label-switching issue. All four of these top-level functions have have standard R-style manuals to explain their usage. 
 
 If you need any more help, please email louise.mcmillan@vuw.ac.nz.
 
